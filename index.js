02993870e83464277eeec2a591dac61a3fd797b9
raymond <mo.die.xi@foocaa.com>
2018-08-15 11:31:52 +0800

    增加主题配置文件

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import FCListView from './lib/list-view'
 import FCItem from './lib/list-item'
 import FCButton from './lib/button'
+import Theme from './lib/theme'
+
 
 module.exports = {
     FCListView,
     FCItem,
-    FCButton
+    FCButton,
+	Theme
 }
