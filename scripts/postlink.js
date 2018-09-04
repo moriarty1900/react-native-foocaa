0dd79b50db9265b121dba93a0369e27d8ee5c9e4
raymond <mo.die.xi@foocaa.com>
2018-09-04 09:32:07 +0800

    no message

--- scripts/postlink.js
+++ scripts/postlink.js
@@ -1,21 +1,20 @@
 const rnpmlink = require('react-native-foocaa-link-scripts');
 
-// Configure Android first.
-//let promise = null
-// if (rnpmlink.android.checkIfAndroidDirectoryExists()) {
-//     console.log('Configuring AppCenter Analytics for Android');
-//     promise = rnpmlink.android.initAppCenterConfig()
-//         .then(() => {
-//             rnpmlink.android.removeAndroidDuplicateLinks();
-//         }).catch((e) => {
-//             console.error(`Could not configure AppCenter for Android. Error Reason - ${e.message}`);
-//             return Promise.resolve();
-//         });
-// } else {
-//     promise = Promise.resolve();
-// }
+Configure Android first.
+let promise = null
+if (rnpmlink.android.checkIfAndroidDirectoryExists()) {
+    console.log('Configuring AppCenter Analytics for Android');
+    promise = rnpmlink.android.initAppCenterConfig()
+        .then(() => {
+            rnpmlink.android.removeAndroidDuplicateLinks();
+        }).catch((e) => {
+            console.error(`Could not configure AppCenter for Android. Error Reason - ${e.message}`);
+            return Promise.resolve();
+        });
+} else {
+    promise = Promise.resolve();
+}
 
-let promise = Promise.resolve();
 // Then iOS even if Android failed.
 if (rnpmlink.ios.checkIfAppDelegateExists()) {
     promise
